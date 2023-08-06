c8454b22315eb618a733a3e560289c69c1d103cb
KrishnaVhatkar <119169766+KrishnaVhatkar@users.noreply.github.com>
2023-08-06 10:06:16 +0530

    first commit

---
+++ module2.mjs
@@ -0,0 +1,8 @@
+const a = 'krishna';
+const b = 'sumit';
+const c = 'ram';
+const d = 'amit';
+export default c;
+export {a};
+export {b};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+// import About from './components/About';
+import Alerts from './components/Alerts';
+import TextForm from './components/textForm';
+
+// import {createBrowserRouter,Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
+// Layouts
+
+
+
 
 function App() {
+  const [mode, setMode] = useState('dark');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode is enabled", 'success');
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  };
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path='/'>
+  //       <Route path='/' element={<TextForm showAlert={showAlert} heading='Enter the Text below' mode={mode} toggleMode={toggleMode} />} />
+  //       <Route path='/about' element={<About />} />
+  //     </Route>
+  //   )
+  // );
+
+
   return (
-   <>
-   
-   </>
-  );
+    <>
+
+      <Navbar title="TextUtils" about="About Textutils" mode={mode} toggleMode={toggleMode} />
+      <Alerts alert={alert} />
+      <TextForm showAlert={showAlert} heading='Enter the Text below' mode={mode} toggleMode={toggleMode} />
+      {/* <RouterProvider router={router} /> */}
+      
+    </>
+  )
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
